237d648f7e7f7d5bce8fb0148631964d77e906a2
kpamapk <kpamapk8@gmail.com>
2025-05-31 18:11:34 +0200

    Create functions.ts

---
+++ src/data/functions.ts
@@ -0,0 +1,10 @@
+export const functions = [
+  { code: "Ne", name: "Extroverted Intuition" },
+  { code: "Ni", name: "Introverted Intuition" },
+  { code: "Se", name: "Extroverted Sensing" },
+  { code: "Si", name: "Introverted Sensing" },
+  { code: "Te", name: "Extroverted Logic" },
+  { code: "Ti", name: "Introverted Logic" },
+  { code: "Fe", name: "Extroverted Ethics" },
+  { code: "Fi", name: "Introverted Ethics" },
+];
