ef1f97c206561c30e505dcb1c5c77a4af0d71ad0
kpamapk <kpamapk8@gmail.com>
2025-05-31 23:52:22 +0200

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,27 +1,5 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
-import { SITE } from "@/config";
-
-export const BLOG_PATH = "src/data/blog";
-
-const blog = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: `./${BLOG_PATH}` }),
-  schema: ({ image }) =>
-    z.object({
-      author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
-      modDatetime: z.date().optional().nullable(),
-      title: z.string(),
-      featured: z.boolean().optional(),
-      draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
-      ogImage: image().or(z.string()).optional(),
-      description: z.string(),
-      canonicalURL: z.string().optional(),
-      hideEditPost: z.boolean().optional(),
-      timezone: z.string().optional(),
-    }),
-});
 
 const types = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/types" }),
@@ -50,4 +28,4 @@ const dichotomies = defineCollection({
   }),
 });
 
-export const collections = { blog, types, functions, dichotomies };
+export const collections = { types, functions, dichotomies };
