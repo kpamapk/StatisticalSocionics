bc5e54b2e8b52503bb3612443215f6a8ad4d1942
kpamapk <kpamapk8@gmail.com>
2025-06-01 05:05:34 +0200

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,22 @@
 import { defineCollection, z } from "astro:content";
 
-const schema = z.object({
+const baseSchema = z.object({
   title: z.string(),
   description: z.string(),
   code: z.string().optional(),
 });
 
+const databaseSchema = baseSchema.extend({
+  image: z.string().refine((path) => path.startsWith("/reddit-images/"), {
+    message: "Image path must start with /reddit-images/",
+  }),
+});
+
 export const collections = {
-  types: defineCollection({ schema }),
-  functions: defineCollection({ schema }),
-  dichotomies: defineCollection({ schema }),
-  database: defineCollection({ schema }),
-  neurosocionics: defineCollection({ schema }), // 👈 ADD THIS
+  types: defineCollection({ schema: baseSchema }),
+  functions: defineCollection({ schema: baseSchema }),
+  dichotomies: defineCollection({ schema: baseSchema }),
+  database: defineCollection({ schema: databaseSchema }),
+  neurosocionics: defineCollection({ schema: baseSchema }),
 };
+
